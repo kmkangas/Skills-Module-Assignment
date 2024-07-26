d2e0c07a924c59d1ee41f96d4b4c9c6c1574baa0
kmkangas <kkangas1@asu.edu>
2024-07-26 14:23:27 -0700

    Setup code in repository

---
+++ MyName.cpp
@@ -0,0 +1,16 @@
+// C++ print name program
+
+#include <iostream> 
+using namespace std; 
+
+int main() 
+{ 
+string name; 
+	
+cout << "Enter your name: "; 
+cin >> name; 
+	
+cout << "So your name is: " << 
+		name; 
+	return 0; 
+}
